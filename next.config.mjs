278631a3a9ed9d09823e712df52b890fd4106c53
Adarsh-oo7 <adarshsarachandran@gmail.com>
2025-05-01 21:18:48 +0530

    dd

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+ 
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +12,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+
+export default nextConfig;
